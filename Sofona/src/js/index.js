7eaa5503ae930fd30d8d662fe8dd58ba8e851512
andreyminaev96 <andrey.minaev.simba@gmail.com>
2018-10-10 14:54:07 +0300

    sofona

---
+++ Sofona/src/js/index.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $('.last-section__right-img svg').click( function(event){
+        event.preventDefault();
+        $('#overlay').fadeIn(400,
+            function(){
+                $('#modal_form')
+                    .css('display', 'flex')
+                    .animate({opacity: 1, top: '25%'}, 200);
+            });
+    });
+
+    $('#modal_close, #overlay').click( function(){
+        $('#modal_form')
+            .animate({opacity: 0, top: '45%'}, 200,
+                function(){
+                    $(this).css('display', 'none');
+                    $('#overlay').fadeOut(400);
+                }
+            );
+    });
+});
